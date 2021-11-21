c3a503625194c2742bbfc82388b0bc940305e6ff
Nathan Graule <solarliner@gmail.com>
2021-11-22 00:02:18 +0100

    feat: make computation tree in REPL

--- repl/src/main.rs
+++ repl/src/main.rs
@@ -1,8 +1,13 @@
-use ariadne::Source;
+use ariadne::{ReportKind, Source};
+use computations::check;
 use parser::parse;
 use rustyline::{Editor, error::ReadlineError};
+use std::ops::Range;
 
 const HISTORYFILE: &'static str = "/tmp/ulp-repl.history";
+
+type SrcId = (String, Range<usize>);
+type Report = ariadne::Report<SrcId>;
 
 fn main() {
     let mut rl = Editor::<()>::new();
@@ -17,7 +22,15 @@ fn main() {
                     err.eprint(("<repl>".to_string(), Source::from(&line))).unwrap();
                 }
                 if let Some(ast) = ast {
-                    println!("{:?}", ast);
+                    match check(ast) {
+                        Ok(comp) => println!("Computation {:#?}", comp),
+                        Err(err) => Report::build(ReportKind::Error, "<repl>", 0)
+                            .with_message(format!("Check error: {}", err))
+                            .with_note("The structure is correct, however ULP could not figure out how to compute the expression.")
+                            .finish()
+                            .eprint(("<repl>".to_string(), Source::from(&line)))
+                            .unwrap(),
+                    }
                 }
             },
             Err(ReadlineError::Interrupted) => {},
